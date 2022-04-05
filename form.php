4e696473b720c04c0fbd133ae1791cd90bb6a345
Joaquin Ahumada <joaquinahumada1998@gmail.com>
2022-04-05 13:10:04 -0300

    contacto formulario

---
+++ form.php
@@ -0,0 +1,16 @@
+<?php
+    
+    $mail = $_POST['email'];
+    $mensaje = $_POST['textarea'];
+    
+ 
+    $para = 'nicolas.seguro@gmail.com';
+    $asunto = 'Este mail fue enviado desde la web';
+
+
+    mail($para, $asunto, utf8_decode($nombre, $mail, $mensaje));
+
+    
+    header('Location:exito.html');
+
+?>
